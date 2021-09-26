25b2785f1f5e2cf1a4e79d2f62525c7d430dfbbb
skiftare <74261401+Skiftare@users.noreply.github.com>
2021-09-26 12:43:24 +0500

    Create 1264.java

---
+++ Timus/1264.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a1 = input.nextInt();
+        int b1 = input.nextInt();
+        System.out.print(a1*(b1+1));
+
+        input.close();
+    }
+}
