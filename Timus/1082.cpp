21c9faf74b698f568abf4f5fa85d909bcc03d1d6
skiftare <74261401+Skiftare@users.noreply.github.com>
2022-03-13 20:24:39 +0500

    Create 1082.cpp

---
+++ Timus/1082.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+const int inf = 1e9+7;
+using namespace std;
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    int n;
+    cin>>n;
+    for(int i = 0;i<n;i++){
+        cout<<i+1<<' '<<endl;
+    }
+    return 0;
+}
