dee12a018c1d6e0930c7db2a985544e9867aa637
skiftare <74261401+Skiftare@users.noreply.github.com>
2022-03-21 17:57:59 +0500

    Create 1336.cpp

---
+++ Timus/1336.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int long long
+const int inf = 1e7+9;
+using namespace std;
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    int n;
+    cin>>n;
+    cout<<n*n<<endl<<n<<endl;
+    return 0;
+}
