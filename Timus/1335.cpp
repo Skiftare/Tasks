e1892ec9f97e179e37b638c1ffa7ce4b0640bba7
skiftare <74261401+Skiftare@users.noreply.github.com>
2022-05-29 10:48:33 +0500

    Create 1335.cpp

---
+++ Timus/1335.cpp
@@ -0,0 +1,28 @@
+// If you copy paste my solution I will murder you in your sleep.
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+signed solve(){
+    int n;
+    cin>>n;
+    cout<<n*n+n<<' '<<n*n+2*n<<' '<<n*n<<endl;
+
+    return 0;
+
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int test = 1;
+    //cin>>test;
+    while(test--){
+        solve();
+    }
+    return 0;
+}
