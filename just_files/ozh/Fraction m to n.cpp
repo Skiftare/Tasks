5085009000ae56ef3d3f9736a66fd33712254239
skiftare <74261401+Skiftare@users.noreply.github.com>
2022-08-13 20:34:20 +0500

    Update Fraction m to n.cpp

--- just_files/ozh/Fraction m to n.cpp
+++ just_files/ozh/Fraction m to n.cpp
@@ -1,1 +1,38 @@
-//
+// If you copy and paste my solution I will murder you in your sleep.
+#include <bits/stdc++.h>
+
+signed main ()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    auto fun = [&] (int a, int b){
+        while(b){
+            a %= b;
+            std::swap(a,b);
+        }
+    };
+
+    int m,a;
+    int n,b;
+
+    std::cin>>m>>n;
+
+    a = abs(m);
+    b = n;
+
+    if(m == 0) {
+        std::cout<<m<<'\n';
+        return 0;
+    }
+    else if(m%n == 0 and m>n) {
+        std::cout<<m/n<<'\n';
+        return 0;
+    }
+
+    fun(a, b);
+    std::cout<<m/a<<':'<<n/a<<'\n';
+    
+    return 0;
+}
