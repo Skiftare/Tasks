3e7a296730bba5972e15bd56839a93e65d31cfa9
skiftare <74261401+Skiftare@users.noreply.github.com>
2022-08-14 09:25:57 +0500

    Update Fraction m to n.cpp

--- just_files/ozh/Fraction m to n.cpp
+++ just_files/ozh/Fraction m to n.cpp
@@ -1,38 +1,37 @@
-// If you copy and paste my solution I will murder you in your sleep.
-#include <bits/stdc++.h>
+#include <iostream>
 
-signed main ()
+signed main()
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
     std::cout.tie(0);
 
-    auto fun = [&] (int a, int b){
-        while(b){
+    auto fun = [&](int a, int b) {
+        while (b) {
             a %= b;
-            std::swap(a,b);
+            std::swap(a, b);
         }
     };
 
-    int m,a;
-    int n,b;
+    int m, a;
+    int n, b;
 
-    std::cin>>m>>n;
+    std::cin >> m >> n;
 
     a = abs(m);
     b = n;
 
-    if(m == 0) {
-        std::cout<<m<<'\n';
+    if (m == 0) {
+        std::cout << m << '\n';
         return 0;
     }
-    else if(m%n == 0 and m>n) {
-        std::cout<<m/n<<'\n';
+    else if (m % n == 0 and m > n) {
+        std::cout << m / n << '\n';
         return 0;
     }
 
     fun(a, b);
-    std::cout<<m/a<<':'<<n/a<<'\n';
-    
+    std::cout << m / a << ':' << n / a << '\n';
+
     return 0;
 }
