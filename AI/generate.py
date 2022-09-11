50287a8276320c40be444801bd3af00ba6f5d84f
skiftare <74261401+Skiftare@users.noreply.github.com>
2022-09-11 17:59:50 +0500

    Update generate.py

--- AI/generate.py
+++ AI/generate.py
@@ -1,23 +1,17 @@
 from main import *
 
-
 try:
     CG = ClassicGram(1)
     CG.restoring()
 except:
     from train import *
-
-
-
-
+    
 print("Please, input first world.")
 s = input()
 print("Please, input length of text")
 len = int(input())
-print(s)
 res = [s]
 for i in range (len):
     generated = CG.generate(res)
     res.append(generated)
-    #print(i)
 for it in res: print(it, end = " ")
